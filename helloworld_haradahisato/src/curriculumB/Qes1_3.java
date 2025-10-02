1be5b362a964831a4499b8cdbf8d80079be92d3b
haradahisato <haradahisato@localhost>
2025-10-02 22:34:02 +0900

    ユーザー名が引っかかった場合再度入力できるように修正 カリキュラムB

--- helloworld_haradahisato/src/curriculumB/Qes1_3.java
+++ helloworld_haradahisato/src/curriculumB/Qes1_3.java
@@ -2,14 +2,17 @@ package curriculumB;
 
 import java.util.Random;
 import java.util.Scanner;
+
 class Qes1_3 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		String name = "";
+		boolean nameValidation = true;
 		boolean keepRunning = true;
 
-		
-			String name = scanner.nextLine();
+		while (nameValidation) {
+			name = scanner.nextLine();
 
 			if (name.length() > 10) { //１０文字以下であれば
 				System.out.println("名前を10文字以内にしてください");
@@ -19,41 +22,41 @@ class Qes1_3 {
 				System.out.println("半角英数字のみで名前を入力してください");
 			} else {
 				System.out.println("ユーザー名『" + name + "』を登録しました");
-				
+				nameValidation = false;
 			}
-			int num = 0;
-			while (keepRunning) {
+		}
+		int num = 0;
+		while (keepRunning) {
 			int input = Integer.parseInt(scanner.nextLine());
-	       
-	       
-	        Random random = new Random();
-	        int randomNumber = random.nextInt(3) + 0;
-	        String[] janken = {"グー","チョキ","パー"};
-	        num++;
-	        System.out.println(name +"の手は"+ janken[input]);
-	        System.out.println("相手の手は"+ janken[randomNumber]);
-	        if(input == randomNumber) {
-	        	System.out.println("DRAW あいこ もう一回しましょう！");
-	        	
-	        }else if(input == 1 && randomNumber == 0) {
-	        	System.out.println("俺の勝ち！");
-	        	System.out.println("負けは次につながるチャンスです！");
-	        	System.out.println("ネバーギブアップ！");
-	        }else if(input == 2 && randomNumber == 1) {
-	        	System.out.println("俺の勝ち！");
-	        	System.out.println("たかがじゃんけん、そう思ってないですか？");
-	        	System.out.println("それやったら次も、俺が勝ちますよ");
-	        }else if(input == 0 && randomNumber == 2) {
-	        	System.out.println("俺の勝ち！");
-	        	System.out.println("なんで負けたか、明日まで考えといてください。");
-	        	System.out.println("そしたら何かが見えてくるはずです");
-	        }else {
-	        	System.out.println("やるやん。");
-	        	System.out.println("次は俺にリベンジさせて");
-	        	keepRunning = false;
+
+			Random random = new Random();
+			int randomNumber = random.nextInt(3) + 0;
+			String[] janken = { "グー", "チョキ", "パー" };
+			num++;
+			System.out.println(name + "の手は" + janken[input]);
+			System.out.println("相手の手は" + janken[randomNumber]);
+			if (input == randomNumber) {
+				System.out.println("DRAW あいこ もう一回しましょう！");
+
+			} else if (input == 1 && randomNumber == 0) {
+				System.out.println("俺の勝ち！");
+				System.out.println("負けは次につながるチャンスです！");
+				System.out.println("ネバーギブアップ！");
+			} else if (input == 2 && randomNumber == 1) {
+				System.out.println("俺の勝ち！");
+				System.out.println("たかがじゃんけん、そう思ってないですか？");
+				System.out.println("それやったら次も、俺が勝ちますよ");
+			} else if (input == 0 && randomNumber == 2) {
+				System.out.println("俺の勝ち！");
+				System.out.println("なんで負けたか、明日まで考えといてください。");
+				System.out.println("そしたら何かが見えてくるはずです");
+			} else {
+				System.out.println("やるやん。");
+				System.out.println("次は俺にリベンジさせて");
+				keepRunning = false;
 			}
-			}
-			System.out.println("勝つまでにかかった合計回数は"+ num + "です");
+		}
+		System.out.println("勝つまでにかかった合計回数は" + num + "です");
 	}
 
 }
